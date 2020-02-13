249d3cf79b958a0ae2c678de422bec23d3816e19
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-02-13 09:40:30 -0600

    :construction: final working Luhn
    final working Luhn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import validator from './validator.js';
 
 const creditCardInput = document.getElementById("credit-card-number");
 const button = document.getElementById("button");
+const goodValidationPage = document.getElementById("validated");
+const notValidatedPage = document.getElementById("not-validated");
 
 button.addEventListener('click', () => {
     creditCardInput.innerText = maskify(creditCardInput);
--- wannapay.js
+++ wannapay.js
@@ -1,14 +1,14 @@
-/* Unit tests
- const creditCardInput = '4056789067542345';
-const creditCardInput = '';
-const creditCardInput = 'ASER4'
-*/
+// User cases
+//  const creditCardInput = '4056789067542345';
+const creditCardInput = '4137894711755904';
+// const creditCardInput = '';
+// const creditCardInput = 'ASER4'
 
 function validateUserInput(creditCardInput) {
     // validates user input is not empty and is a number
-    if (creditCardArray.length === 0) {
+    if (creditCardInput.length === 0) {
         console.log('Por favor ingrese los dígitos de su tarjeta')
-    } else if (creditCardArray.length > 0) {
+    } else if (creditCardInput.length > 0) {
         const digitsRegEx = /^[0-9]*$/.test(creditCardInput)
         if (digitsRegEx === true) {
             turnStringToArray(creditCardInput)
@@ -17,8 +17,6 @@ function validateUserInput(creditCardInput) {
         }
     }
 }
-
-validateUserInput(creditCardInput);
 
 function turnStringToArray(creditCardInput) {
     // converts user input to an array of numbers
@@ -30,8 +28,6 @@ function turnStringToArray(creditCardInput) {
     return creditCardArray;
 }
 
-const newArray = turnStringToArray(creditCardInput);
-
 function reversingArray(arr) {
     // reverses the array
     const reversedArray = [];
@@ -41,38 +37,46 @@ function reversingArray(arr) {
     return reversedArray;
 }
 
-const validationArray = reversingArray(newArray);
-
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-// esto debería ser una función o varias??
-for (let i = 0; i <= myArray.length - 1; i++) {
-    let newIndexValue = 0;
-    let temporarySum = [];
-    let resizedDigit = 0;
-    if (i % 2 === 0) {
-        validationArray.push(myArray[i]);
-    } else {
-        newIndexValue = myArray[i] * 2;
-        if (newIndexValue >= 10) {
-            //descomponer un número en dígitos
-            newIndexValue = newIndexValue.toString();
-            for (let i = 0, len = newIndexValue.length; i < len; i += 1) {
-                temporarySum.push(+newIndexValue.charAt(i));
-            }
-            resizedDigit = (temporarySum.reduce(reducer));
-            validationArray.push(resizedDigit);
+// // esto debería ser una función o varias??
+function preparingLuhn() {
+    let validationArray = [];
+    for (let i = 0; i <= newReversedArray.length - 1; i++) {
+        let newIndexValue = 0;
+        let temporarySum = [];
+        let resizedDigit = 0;
+        if (i % 2 === 0) {
+            validationArray.push(newReversedArray[i]);
         } else {
-            validationArray.push(newIndexValue);
+            newIndexValue = newReversedArray[i] * 2;
+            if (newIndexValue >= 10) {
+                //descomponer un número en dígitos
+                newIndexValue = newIndexValue.toString();
+                for (let i = 0, len = newIndexValue.length; i < len; i += 1) {
+                    temporarySum.push(+newIndexValue.charAt(i));
+                }
+                resizedDigit = (temporarySum.reduce(reducer));
+                validationArray.push(resizedDigit);
+            } else {
+                validationArray.push(newIndexValue);
+            }
         }
     }
+    return validationArray;
 }
 
 function validatingLuhn() {
-    let finalValue = (myArray.reduce(reducer));
+    let finalValue = (preparedArray.reduce(reducer));
     if (finalValue % 10 === 0) {
         console.log('Su tarjeta es válida, estás siendo redirigido al sitio de pago')
     } else {
         console.log('Oh no! Tu tarjeta no ha sido reconocida. Quieres intentarlo de nuevo?')
     }
 }
+
+validateUserInput(creditCardInput);
+const newArray = turnStringToArray(creditCardInput);
+const newReversedArray = reversingArray(newArray);
+const preparedArray = preparingLuhn(newReversedArray);
+validatingLuhn(preparedArray);
