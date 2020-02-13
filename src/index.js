7128707188885776ed40b122bda28f3ae0469068
Michelle Pineda <michelle.pineda.rivas@gmail.com>
2020-02-13 08:20:33 -0600

    :construction: Added more style and JS
    began adding more CSS and importing modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
 
+const creditCardInput = document.getElementById("credit-card-number");
+const button = document.getElementById("button");
+
+button.addEventListener('click', () => {
+    creditCardInput.innerText = maskify(creditCardInput);
+});
+
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,12 @@
 const validator = {
-  // ...
+    // ...
+
+
+    // aquí debo cambiar por el selector del DOM
+    validator.maskify(creditCardNumber) {
+        creditCardInput.replace(/.(?=.{4,}$)/g, '#');
+    }
+
 };
 
 export default validator;
